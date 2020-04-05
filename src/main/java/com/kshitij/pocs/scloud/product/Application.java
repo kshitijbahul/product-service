8fe2f1147676197596d5d43081a5fc9c15066a33
KshitijBahul <chunks.3@gmail.com>
2020-04-05 23:26:57 +0530

    Added Eureka Client and ribbon

--- src/main/java/com/kshitij/pocs/scloud/product/Application.java
+++ src/main/java/com/kshitij/pocs/scloud/product/Application.java
@@ -2,10 +2,20 @@ package com.kshitij.pocs.scloud.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Application {
     public static void main(String[] args) {
             SpringApplication.run(Application.class,args);
+    }
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 }
---
+++ src/main/java/com/kshitij/pocs/scloud/product/client/ProductClient.java
@@ -0,0 +1,23 @@
+package com.kshitij.pocs.scloud.product.client;
+
+import com.kshitij.pocs.scloud.product.model.Product;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@Slf4j
+public class ProductClient {
+
+    @Autowired
+    private RestTemplate restTemplate;
+    @GetMapping("/client/product/{id}")
+    ResponseEntity<Product> getProduct(@PathVariable("id") Long id){
+        log.info("Entered the get Product Service");
+        return restTemplate.getForEntity("http://PRODUCT-SERVICE"+"/product/1",Product.class);
+    }
+}
