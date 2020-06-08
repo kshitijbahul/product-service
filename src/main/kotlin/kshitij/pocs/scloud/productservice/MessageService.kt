07ffacb0bb867eb5999de167d977289414c2de61
KshitijBahul <chunks.3@gmail.com>
2020-06-08 19:38:02 +0530

    Added Gson,Sending Object as Json

--- src/main/kotlin/kshitij/pocs/scloud/productservice/MessageService.kt
+++ src/main/kotlin/kshitij/pocs/scloud/productservice/MessageService.kt
@@ -1,9 +1,15 @@
 package kshitij.pocs.scloud.productservice
 
+import com.google.gson.Gson
 import org.slf4j.LoggerFactory
+import org.springframework.amqp.core.MessagePropertiesBuilder
 import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.MediaType
+import org.springframework.messaging.support.MessageBuilder
 import org.springframework.stereotype.Service
+import kotlin.random.Random
 
 @Service
 class MessageProducerService(val rabbitTemplate: RabbitTemplate){
@@ -13,8 +19,21 @@ class MessageProducerService(val rabbitTemplate: RabbitTemplate){
     @Value("\${rabbit.product.topicExchange}")
     lateinit var topicName:String
 
+    @Autowired
+    lateinit var gson:Gson;
+
     fun sendMessage(product: Product,isDeleted: Boolean,routingKey:String){
-        logger.info("Going to send message to Rabbit on the the topic {}",topicName)
-        rabbitTemplate.convertAndSend(topicName,routingKey,ProductUpdateMessage(product,isDeleted))
+        logger.info("Going to send message to Rabbit on the the topic $topicName")
+        val messageProperties = MessagePropertiesBuilder.newInstance()
+                                    .setContentType(MediaType.APPLICATION_JSON_VALUE)
+                                    .setMessageId(Random(Int.MAX_VALUE).toString()).build()
+
+       /*val messageBuilder = MessageBuilder
+                                    .withBody()
+                                    //.andProperties(messageProperties)
+                                    .build()*/
+        //rabbitTemplate.convertAndSend(topicName,routingKey,gson.toJson(ProductUpdateMessage(product,isDeleted)).toByteArray())
+
+        rabbitTemplate.convertAndSend(topicName,routingKey, MessageBuilder.withPayload(gson.toJson(ProductUpdateMessage(product, isDeleted))).build())
     }
 }
--- src/main/kotlin/kshitij/pocs/scloud/productservice/ProductServiceApplication.kt
+++ src/main/kotlin/kshitij/pocs/scloud/productservice/ProductServiceApplication.kt
@@ -1,15 +1,24 @@
 package kshitij.pocs.scloud.productservice
 
+import com.google.gson.Gson
+import org.springframework.amqp.rabbit.connection.ConnectionFactory
+import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.context.config.annotation.RefreshScope
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
 @EnableDiscoveryClient//To be discovered by Eureka
 @RefreshScope
-class ProductServiceApplication
+open class ProductServiceApplication
 
 fun main(args: Array<String>){
     runApplication<ProductServiceApplication>(*args)
+}
+@Bean
+fun gson(): Gson{
+    return Gson()
 }
